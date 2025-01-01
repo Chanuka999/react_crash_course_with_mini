046a07be9af27b1600fe800ed24c82350ae02429
CHANUKA RANDITHA <155728444+Chanuka999@users.noreply.github.com>
2025-01-01 15:38:17 +0000

    update

--- src/Home.js
+++ src/Home.js
@@ -2,47 +2,62 @@ const Home = () => {
   return (
     <div className="Home">
       <h2>Homepage</h2> <br></br>
-      <Example />
+      <ExampleTwo name="chanuka" age="23" job="software Engineer" />
+      <ExampleTwo name="kamal" age="25" job="eletrical Engineer" />
+      <ExampleTwo name="gamlath" age="31" job="civil enginner" />
     </div>
   );
 };
 
-const Example = () => {
+const ExampleTwo = (props) => {
   return (
     <div>
-      <div>
-        <h3>name:chanuka</h3>
-        <br />
-        <h3>age:23</h3>
-        <br />
-        <h3>job:softwre engineer</h3>
-        <hr />
-      </div>
-
-      <div>
-        <h3>name:kamal</h3>
-        <br />
-        <h3>age:25</h3>
-        <br />
-        <h3>job:electrical engineer</h3> <hr />
-      </div>
-
-      <div>
-        <h3>name:perera</h3>
-        <br />
-        <h3>age:24</h3>
-        <br />
-        <h3>job:civil engineer</h3> <hr />
-      </div>
-
-      <div>
-        <h3>name:gamlath</h3>
-        <br />
-        <h3>age:20</h3>
-        <br />
-        <h3>job:softwre engineer</h3> <hr />
-      </div>
+      <h3>Name:{props.name}</h3>
+      <br />
+      <h3>Age:{props.age}</h3>
+      <br />
+      <h3>Job:{props.job}</h3>
+      <hr />
     </div>
   );
 };
+
+// const Example = () => {
+//   return (
+//     <div>
+//       <div>
+//         <h3>name:chanuka</h3>
+//         <br />
+//         <h3>age:23</h3>
+//         <br />
+//         <h3>job:softwre engineer</h3>
+//         <hr />
+//       </div>
+
+//       <div>
+//         <h3>name:kamal</h3>
+//         <br />
+//         <h3>age:25</h3>
+//         <br />
+//         <h3>job:electrical engineer</h3> <hr />
+//       </div>
+
+//       <div>
+//         <h3>name:perera</h3>
+//         <br />
+//         <h3>age:24</h3>
+//         <br />
+//         <h3>job:civil engineer</h3> <hr />
+//       </div>
+
+//       <div>
+//         <h3>name:gamlath</h3>
+//         <br />
+//         <h3>age:20</h3>
+//         <br />
+//         <h3>job:softwre engineer</h3> <hr />
+//       </div>
+//     </div>
+//   );
+// };
 export default Home;
