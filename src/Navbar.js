228af9cd356d7b3616a9f522a394eac40934f615
CHANUKA RANDITHA <155728444+Chanuka999@users.noreply.github.com>
2025-01-01 13:34:06 +0000

    create new file

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <h1>The corder web app</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">New App</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
