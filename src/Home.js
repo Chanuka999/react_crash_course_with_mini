92a45ca8f2946f3b8574fa68da553c65af434951
CHANUKA RANDITHA <155728444+Chanuka999@users.noreply.github.com>
2025-01-01 13:42:23 +0000

    add details

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,48 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="Home">
+      <h2>Homepage</h2> <br></br>
+      <Example />
     </div>
   );
 };
 
+const Example = () => {
+  return (
+    <div>
+      <div>
+        <h3>name:chanuka</h3>
+        <br />
+        <h3>age:23</h3>
+        <br />
+        <h3>job:softwre engineer</h3>
+        <hr />
+      </div>
+
+      <div>
+        <h3>name:kamal</h3>
+        <br />
+        <h3>age:25</h3>
+        <br />
+        <h3>job:electrical engineer</h3> <hr />
+      </div>
+
+      <div>
+        <h3>name:perera</h3>
+        <br />
+        <h3>age:24</h3>
+        <br />
+        <h3>job:civil engineer</h3> <hr />
+      </div>
+
+      <div>
+        <h3>name:gamlath</h3>
+        <br />
+        <h3>age:20</h3>
+        <br />
+        <h3>job:softwre engineer</h3> <hr />
+      </div>
+    </div>
+  );
+};
 export default Home;
